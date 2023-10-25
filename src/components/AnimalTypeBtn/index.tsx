8e4332800a21ce7edb599bbf3d6a7bcb51c4656c
badahertz52 <badahertz52@gmail.com>
2023-10-25 14:55:03 +0900

    [feat] Make feat that change type of animal when click animalTypeBtn

--- src/components/AnimalTypeBtn/index.tsx
+++ src/components/AnimalTypeBtn/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import styles from "./style.module.scss";
 import { AnimalType } from "../../type";
+import { animalProfile } from "../../data/state";
+import { useSetRecoilState } from "recoil";
 
 type AnimalTypeBtnProps = {
   type: AnimalType;
 };
 function AnimalTypeBtn({ type }: AnimalTypeBtnProps) {
   const animalIcon = type === "dog" ? "🐶" : type === "cat" ? "🐱" : "🐼";
-
+  const setAnimalProfileState = useSetRecoilState(animalProfile);
+  const handleClick = () => {
+    setAnimalProfileState((prev) => ({ ...prev, type: type }));
+  };
   return (
-    <button className={styles.btn} title={type}>
+    <button className={styles.btn} title={type} onClick={handleClick}>
       {animalIcon}
     </button>
   );
